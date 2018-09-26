a78745cec8f55dc1054932b7e8d3d7db45192cc2
Merline Belizaire <merline@noukod.com>
2018-09-26 17:47:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  my_collect(array) do |name|
+    name.split(" ").first
+  end
 end
 
 def summon_captain_planet# code an argument here
